1d19bc00cb7607b6993959f9e512c618758a070e
karine <karine.porto@gmail.com>
2024-02-19 16:50:39 +0000

    tela marcas

---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Busca de marcas</div>
+
+                <div class="card-body">
+                    <div class="row g-3">
+                        <div class="col mb-3">
+
+                            <input-container-component titulo="ID" id="inputId" id-help="idHelp" texto-ajuda="Opcional. Informe o id do registro">
+                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                            </input-container-component>              
+                        </div>   
+
+                        <div class="col mb-3">
+
+                            <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o nome da marca.">
+                                <input type="text" class="form-control" id="inputNome" aria-describedby="inputNome" placeholder="Nome da marca">
+                            </input-container-component> 
+                        </div>      
+                    </div>
+                </div>
+
+                <div class="card-footer d-flex">
+                    <button type="submit" class="btn btn-primary btn-sm ms-auto">Pesquisar</button>
+                </div> 
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import InputContainer from './InputContainer.vue'
+export default {
+  components: { InputContainer },
+    
+}
+</script>
