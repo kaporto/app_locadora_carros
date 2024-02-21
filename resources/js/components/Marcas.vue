1c5eca4cbef82c0b74a1bbd5a7b177e071c49281
karine <karine.porto@gmail.com>
2024-02-21 11:56:48 +0000

    evitando dependencias no vue

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -105,7 +105,7 @@
                 nomeMarca: '',
                 arquivoImagem: [],
                 transacaoStatus: '',
-                transacaoDetalhes: []
+                transacaoDetalhes: {}
             }
         },
         methods: {
@@ -129,12 +129,17 @@
                 axios.post(this.urlBase, formData, config)
                     .then(response => {
                         this.transacaoStatus = 'adicionado'
-                        this.transacaoDetalhes = response
+                        this.transacaoDetalhes = {
+                            mensagem : 'ID do registro: ' + response.data.id
+                        }
                         console.log(response)
                     })
                     .catch(errors => {
                         this.transacaoStatus = 'erro'
-                        this.transacaoDetalhes = errors.response
+                        this.transacaoDetalhes = {
+                            mensagem: errors.response.data.message,
+                            dados: errors.response.data.errors
+                        }
                         //console.log(errors.response.data.message)
                     })
             }
